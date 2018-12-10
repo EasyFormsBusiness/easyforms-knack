54c754a942fc411b2b0bbd63f2df2dc314b8a945
Taylor Kettle <tin.teapot@gmail.com>
2018-12-10 15:52:04 +1300

    moving debug search log into while loop

--- src/classes/knack.js
+++ src/classes/knack.js
@@ -1,6 +1,6 @@
-const fetch = require("node-fetch");
-const qs = require("qs");
-const regeneratorRuntime = require("regenerator-runtime");
+const fetch = require("node-fetch")
+const qs = require("qs")
+const regeneratorRuntime = require("regenerator-runtime")
 
 // Global (static) varibles (just primatives!)
 
@@ -12,59 +12,59 @@ class Knack {
    * @memberof Knack
    */
   constructor(apiKey, applicationId) {
-    this.debug = false;
-    this.apiKey = apiKey;
-    this.applicationId = applicationId;
-    this.baseUrl = "https://api.knack.com/v1/objects";
+    this.debug = false
+    this.apiKey = apiKey
+    this.applicationId = applicationId
+    this.baseUrl = "https://api.knack.com/v1/objects"
     this.headers = {
       "X-Knack-Application-Id": this.applicationId,
       "X-Knack-REST-API-KEY": this.apiKey,
       "Content-Type": "application/json"
-    };
+    }
   }
 
   async update(objectNo, recordId, update) {
     // console.log("Updating", objectNo, recordId, JSON.stringify(update));
 
-    const url = `${this.baseUrl}/${objectNo}/records/${recordId}`;
+    const url = `${this.baseUrl}/${objectNo}/records/${recordId}`
 
     try {
       let response = await (await fetch(url, {
         headers: this.headers,
         method: "PUT",
         body: JSON.stringify(update)
-      })).json();
-      return response;
+      })).json()
+      return response
     } catch (error) {
-      throw Error(error.message);
+      throw Error(error.message)
     }
   }
 
   async create(objectNo, body, retry = 0) {
-    if (!objectNo) throw new Error("You must pass an object number");
-    if (!body) throw new Error("You must pass a body");
+    if (!objectNo) throw new Error("You must pass an object number")
+    if (!body) throw new Error("You must pass a body")
 
     // console.log("Creating", objectNo, JSON.stringify(body));
 
-    const url = `${this.baseUrl}/${objectNo}/records`;
-    let response;
+    const url = `${this.baseUrl}/${objectNo}/records`
+    let response
     try {
       response = await (await fetch(url, {
         headers: this.headers,
         method: "POST",
         body: JSON.stringify(body)
-      })).json();
+      })).json()
     } catch (error) {
-      retry += 1;
+      retry += 1
       if (retry < 5) {
-        await sleep(getRandomInt(1000 * 10));
-        this.create(objectNo, body, retry);
+        await sleep(getRandomInt(1000 * 10))
+        this.create(objectNo, body, retry)
       } else {
-        throw new Error(error);
+        throw new Error(error)
       }
     }
 
-    return response;
+    return response
   }
 
   /**
@@ -79,43 +79,43 @@ class Knack {
   async upsert(objectNo, object, searchObject) {
     // console.log("Upsert", objectNo, JSON.stringify(object));
 
-    if (!searchObject) searchObject = object;
+    if (!searchObject) searchObject = object
 
     let filters = Object.keys(searchObject).reduce(
       (obj, field) => {
-        obj.rules.push({ field: field, operator: "is", value: object[field] });
-        return obj;
+        obj.rules.push({ field: field, operator: "is", value: object[field] })
+        return obj
       },
       { match: "and", rules: [] }
-    );
+    )
 
-    let [record] = await this.search(objectNo, filters);
+    let [record] = await this.search(objectNo, filters)
 
     if (record) {
-      return await this.update(objectNo, record.id, object);
+      return await this.update(objectNo, record.id, object)
     } else {
-      return await this.create(objectNo, object);
+      return await this.create(objectNo, object)
     }
   }
 
   async delete(objectNo, id, retry = 0) {
-    const url = `${this.baseUrl}/${objectNo}/records/${id}`;
-    let response;
+    const url = `${this.baseUrl}/${objectNo}/records/${id}`
+    let response
     try {
       response = await (await fetch(url, {
         headers: this.headers,
         method: "DELETE"
-      })).json();
+      })).json()
     } catch (error) {
-      retry += 1;
+      retry += 1
       if (retry < 5) {
-        this.delete(objectNo, id, retry);
+        this.delete(objectNo, id, retry)
       } else {
-        throw new Error(error);
+        throw new Error(error)
       }
     }
 
-    return response;
+    return response
   }
 
   async search(
@@ -125,12 +125,12 @@ class Knack {
     sortOrder = false,
     retry = 1
   ) {
-    let page = 1;
-    let totalPages = 1;
-    let allRecords = [];
+    let page = 1
+    let totalPages = 1
+    let allRecords = []
 
     if (sortField ? !sortOrder : sortOrder) {
-      throw new Error("Must specify both sortField and sortOrder!");
+      throw new Error("Must specify both sortField and sortOrder!")
     }
 
     const url = `${
@@ -141,92 +141,92 @@ class Knack {
       sortField && sortOrder
         ? `&sort_field=${sortField}&sort_order=${sortOrder}`
         : ""
-    }`;
-
-    if (this.debug) console.log(url);
+    }`
 
     try {
       while (page <= totalPages) {
+        if (this.debug) console.log(url)
+
         let { records, total_pages } = await (await fetch(url, {
           headers: this.headers
-        })).json();
+        })).json()
 
-        allRecords = allRecords.concat(records);
-        totalPages = total_pages;
-        page += 1;
+        allRecords = allRecords.concat(records)
+        totalPages = total_pages
+        page += 1
       }
 
-      return allRecords;
+      return allRecords
     } catch (error) {
       if (retry <= 5) {
-        let retryDelay = Math.random() * 10 * 1000;
+        let retryDelay = Math.random() * 10 * 1000
         console.error(
           `Error searching in knack, retry no. ${retry} in ${retryDelay /
             1000} seconds...`
-        );
-        await sleep(retryDelay);
+        )
+        await sleep(retryDelay)
         return this.search(
           objectNo,
           filters,
           sortField,
           sortOrder,
           (retry += 1)
-        );
+        )
       } else {
-        console.error("Error searching in knack", error);
-        throw Error(error);
+        console.error("Error searching in knack", error)
+        throw Error(error)
       }
     }
   }
 
   async get(objectNo, retry = 1) {
-    let allRecords = [];
-    let page = 1;
-    let totalPages = 1;
+    let allRecords = []
+    let page = 1
+    let totalPages = 1
 
     try {
       while (page <= totalPages) {
         let { records, total_pages } = await (await fetch(
           `${this.baseUrl}/${objectNo}/records?page=${page}&rows_per_page=1000`,
           { headers: this.headers }
-        )).json();
+        )).json()
         // console.log(Object.keys(result));
 
-        totalPages = total_pages;
-        page += 1;
-        allRecords = allRecords.concat(records);
+        totalPages = total_pages
+        page += 1
+        allRecords = allRecords.concat(records)
       }
     } catch (error) {
-      console.error(error);
+      console.error(error)
       if (retry < 2) {
-        retry += 1;
-        this.get(objectNo, retry);
+        retry += 1
+        this.get(objectNo, retry)
       }
     }
-    return allRecords;
+    return allRecords
   }
 
   async getOne(objectNo, id, retry = 1) {
     try {
       return await (await fetch(`${this.baseUrl}/${objectNo}/records/${id}`, {
         headers: this.headers
-      })).json();
+      })).json()
     } catch (error) {
-      console.error(error);
+      console.error(error)
       if (retry < 2) {
-        retry += 1;
-        this.getOne(objectNo, id, retry);
+        retry += 1
+        this.getOne(objectNo, id, retry)
       }
     }
   }
 }
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max))
 }
 
-module.exports = Knack;
+module.exports = Knack
